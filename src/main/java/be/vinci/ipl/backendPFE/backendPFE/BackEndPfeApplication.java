72cf3acab8cd47edfc0628e6e335359a8e23df08
Quentin <quentin.leveque@student.vinci.be>
2022-12-07 19:48:29 +0100

    "basic spring boot app"

---
+++ src/main/java/be/vinci/ipl/backendPFE/backendPFE/BackEndPfeApplication.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.backendPFE.backendPFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vinci/ipl/backendPFE/backendPFE/BackEndPfeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.backendPFE.backendPFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
